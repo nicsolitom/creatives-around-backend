6562093f9030666e862c428f4cf60ae653af8af7
Nic Solitom <nicsolitom@gmail.com>
2020-04-04 01:30:26 +0200

    Allowing cross origin access from frontend.

--- api/server.js
+++ api/server.js
@@ -15,6 +15,22 @@ mongoose
     })
     .then(() => console.log(`MongoDB Atlas connected`));
 
+//? Allow cross origin access from frontend:
+api.use((req, res, next) => {
+    res.set('ACCESS-CONTROL-ALLOW-ORIGIN', 'http://localhost:5000');
+    res.set('ACCESS-CONTROL-ALLOW-CREDENTIALS', 'true');
+    res.set(
+      'ACCESS-CONTROL-ALLOW-HEADERS',
+      'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+    );
+    res.set(
+      'ACCESS-CONTROL-ALLOW-METHODS',
+      'GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS'
+    );
+    next();
+  });
+  
+
 //? Routes:
 api.get('/', (req, res, next) => {
     console.log(`Home route called`);
