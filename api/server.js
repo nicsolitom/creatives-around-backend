c17ada0bffd3c605c494544e107b0950f73c3896
Nic Solitom <nicsolitom@gmail.com>
2020-04-04 00:25:07 +0200

    Added home route

--- api/server.js
+++ api/server.js
@@ -2,5 +2,11 @@ const express = require('express');
 const api = express();
 const mongoose = require('mongoose');
 
+//? Routes:
+api.get('/', (req, res, next) => {
+    console.log(`Home route called`);
+    res.send(`Home route`)
+});
+
 const port = 3000;
 api.listen(port, () => console.log(`Listening on port ${port}`));
