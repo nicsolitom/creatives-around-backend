99c6acfe54d79d55debe59de406cd8be470d5d60
Nic Solitom <nicsolitom@gmail.com>
2020-04-04 01:28:09 +0200

    Connected to MongoDB Atlas

--- api/server.js
+++ api/server.js
@@ -4,6 +4,17 @@ const mongoose = require('mongoose');
 
 const UsersRegList = require('./usersRegModel');
 
+//? Connecting with MongoDB Atlas:
+const caUser =
+'mongodb+srv://caUser:creativesaroundnicmor@userscreativesaround-dldct.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose
+    .connect(caUser, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log(`MongoDB Atlas connected`));
+
 //? Routes:
 api.get('/', (req, res, next) => {
     console.log(`Home route called`);
